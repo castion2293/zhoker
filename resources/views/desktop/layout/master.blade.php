52be690e41f5c23ba4c4d62c019808318307f6fb
castion2293 <castion2293@yahoo.com.tw>
2016-12-03 14:51:29 -0700

    10

--- resources/views/desktop/layout/master.blade.php
+++ resources/views/desktop/layout/master.blade.php
@@ -5,15 +5,15 @@
     <meta charset="UTF-8">
     <title>Zhoker 作客 居家美食 @yield('title')</title>
     <link rel="stylesheet" href="{{ URL::to('css/w3.css') }}">
-    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <link rel="stylesheet" href="{{ URL::to('css/main.css') }}">
     <link rel="stylesheet" href="{{ URL::to('css/component.css') }}"><!--side direction dot-->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" /><!--Select-2-->
     <link rel="stylesheet" href="{{ URL::to('css/bootstrap-datetimepicker.min.css') }}"><!--Bootstrap DateTimePicker-->
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
     <script src="{{ URL::to('js/0609.js') }}"></script>
     <script type="text/javascript" src="{{ URL::to('js/JQ.js') }}"></script>
     <script src="{{ URL::to('js/parsley.min.js') }}"></script> <!--Parsley Validation-->
