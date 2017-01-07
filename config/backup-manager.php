7eb6669a9f27907e2f9ef45f70d5fb111ac84949
castion2293 <castion2293@yahoo.com.tw>
2017-01-06 21:43:00 -0700

    schedule run8

--- config/backup-manager.php
+++ config/backup-manager.php
@@ -11,7 +11,7 @@ return [
         'secret' => env('S3_SECRET'),
         'region' => env('S3_REGION'),
         'bucket' => 'zhoker',
-        'root'   => '',
+        'root'   => '/database_backups/',
     ],
     'gcs' => [
         'type' => 'Gcs',
