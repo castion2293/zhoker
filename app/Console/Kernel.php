e3be51a3e823be1202adf1e976c08edab08b106d
castion2293 <castion2293@yahoo.com.tw>
2017-01-06 21:38:12 -0700

    schedule run7

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->command(
             //"db:backup --database=mysql --destination=s3 --destinationPath=" . Carbon::now() . ".sql --compression=gzip"
-            "db:backup --database=mysql --destination=s3 --destinationPath=" . Carbon::now() . ".sql --compression=gzip"
+            "db:backup --database=mysql --destination=s3 --destinationPath=" . date("Y-m-d", Carbon::now()) . ".sql --compression=gzip"
             //"db:backup --database=mysql --destination=s3 --destinationPath=data.sql --compression=gzip"
             )
             ->everyMinute();
