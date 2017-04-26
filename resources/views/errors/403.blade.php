cbcbf05a161b1ef19c0c490e1650be3399fefb58
castion2293 <castion2293@yahoo.com.tw>
2017-04-25 21:01:43 -0600

    add 404 not found blade

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -40,7 +40,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Be right back.</div>
+                <div class="title">You don't have authorized.</div>
             </div>
         </div>
     </body>
